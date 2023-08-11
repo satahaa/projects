d55f3e3b4cd5c1fdb08ab3b7310cdf69aa3cd4ff
satahaa <satahaa12345@gmail.com>
2023-08-12 00:36:34 +0600

    Realloc done

--- C/heapMem.c
+++ C/heapMem.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void){
-    int *list = malloc(3 * sizeof(int)); //Allocate me 3, 4 bytes of memory.
+int main(void) {
+    int *list = malloc(3 * sizeof(int)); // Allocate memory for 3 integers
 
-    if (list == NULL ){
-        free(list);
-        return 1; //If memory is not available return an error code.
+    if (list == NULL) {
+        return 1; // Return an error code if memory allocation fails
     }
-    //The code does the math for me and figures out which 3 memory chuncks are side to side so that it can store my given integers.
+
     list[0] = 0;
     list[1] = 5;
-    list[2]= 10;
+    list[2] = 10;
 
     printf("list:\n");
 
-    for (int i = 0; i < 4; i++ ){
-        printf("%i\n", list[i]); //Prints out the numbers in new line by iterating through the memory pointers. 
+    for (int i = 0; i < 3; i++) {
+        printf("%i\n", list[i]);
     }
 
-    //time passes
+    int *tmp = realloc(list, 5 * sizeof(int)); // Reallocate memory for 5 integers
 
-    int *tmp = malloc(4 * sizeof(int)); //We make a temporary memory pointer and make it bigger than before so that we can dynamically update our memory chunk 
-    if ( tmp == NULL){
-        free(tmp);
-        return 1; //Same as before
+    if (tmp == NULL) {
+        free(list);
+        return 1; // Return an error code if memory reallocation fails
     }
-    for (int i = 0; i < 3; i++){
-        tmp[i] = list[i]; //Copies the stored data in the list array to the new temporary array.
-    }
-    tmp[3] = 15; //Adds the new data in the next chunk
-    free(list);
-    list = tmp; //Copy the tempory pointer to the original name.
-    
+
+    tmp[3] = 15;
+    tmp[4] = 20;
+
+    // Reassign the 'tmp' pointer back to 'list'
+    list = tmp;
 
     printf("Modified list:\n");
-    
-    for (int i = 0; i < 4; i++ ){
-        printf("%i\n", list[i]); //Prints through the new list.
+
+    for (int i = 0; i < 5; i++) {
+        printf("%i\n", list[i]); // Print from 'list', which now holds the modified data
     }
-    
-    free(list); //Frees the list after it is done working.
+
+    // No need to free 'list' here, since 'tmp' has been assigned to it
+
+    free(list); // Free the final allocated memory
+
+    return 0;
 }
