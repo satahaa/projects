a51bcc74a783d78002f568e01bda5586778aa8e5
SA Tahaa <satahaa12345@gmail.com>
2024-07-05 00:46:36 +0600

    yay

--- wow.cpp
+++ wow.cpp
@@ -6,57 +6,30 @@ typedef long long int i32;
 #define sortall(a) sort(a.begin(), a.end())
 #define revall(a) reverse(a.begin(), a.end())
 #define MOD 1e9 + 7
-#define Yes cout << "Yes" << endl;
 #define YES cout << "YES" << endl;
-#define No cout << "No" << endl;
 #define NO cout << "NO" << endl;
 #define pb push_back
+const int di[] = {-1, 1, 0, 0};
+const int dj[] = {0, 0, -1, 1};
 
-void sol(int t)
-{    
-    int n; cin >> n;
-    string s; cin >> s;
-    unordered_map<char, int> freq;
-
-    // Count frequency of each character
-    for (char c : s) {
-        freq[c]++;
+void sol(int tc) {
+    int n, k; cin >> n >> k;
+    if (k == 1) {
+        cout << n << endl;
+        return;
     }
 
-    int cost = 0;
-    
-    // Separate counts for lowercase and uppercase
-    unordered_map<char, int> lower_counts;
-    unordered_map<char, int> upper_counts;
-
-    for (char c = 'a'; c <= 'z'; ++c) {
-        lower_counts[c] = freq[c];
-        upper_counts[toupper(c)] = freq[toupper(c)];
-    }
-
-    // Calculate the minimal cost to make it palindrome
-    for (char c = 'a'; c <= 'z'; ++c) {
-        char lc = c;
-        char uc = toupper(c);
-        int pairs = min(lower_counts[lc], upper_counts[uc]);
-        int remaining_lower = lower_counts[lc] - pairs;
-        int remaining_upper = upper_counts[uc] - pairs;
-
-        // Cost of converting remaining_lower and remaining_upper to pairs
-        cost += (remaining_lower + remaining_upper) * 2; // Change cost 2 per change
-    }
-
-    cout << cost << endl;
+    cout << n + k - 1<< endl;
+    return;
 }
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
- 
+
     int tc = 1;
     cin >> tc;
-    for (int i = 1; i <= tc; i++) 
+    for (int i = 1; i <= tc; i++)
         sol(i);
     return 0;
 }
