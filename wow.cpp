00186cecb2683acb61d5fee6a96162427a677bd2
SA Tahaa <satahaa12345@gmail.com>
2024-08-11 02:39:15 +0600

    a

--- wow.cpp
+++ wow.cpp
@@ -4,8 +4,10 @@ typedef long long int i32;
 #define endl '\n'
 #define l(i, n) for (int i = 0; i < n; i++)
 #define sorted(a) sort(a.begin(), a.end())
+#define rsorted(a) sort(a.rbegin(), a.rend())
 #define reversed(a) reverse(a.begin(), a.end())
 #define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(). x.rend()
 #define MOD 1e9 + 7
 #define YES cout << "YES" << endl
 #define NO cout << "NO" << endl
@@ -15,20 +17,21 @@ const int dj[] = {0, 0, -1, 1};
 
 void sol(int tc) {
     int n; cin >> n;
-    vector<int> a(n);
-    int sum  = 0;
-    bool e = false, o = false;    
-    l (i, n) {
-        cin >> a[i];
-        sum += a[i];
-        if (a[i] % 2 == 0) e = true;
-        else o = true;
+    unordered_map<char, int> mp{
+        {'A' , 0},
+        {'B', 0},
+        {'C', 0},
+        {'D', 0}
+    };
+    string s; cin >> s;
+    for (char c : s) if (c != '?') mp[c]++;
+    int ans = 0;
+    for (auto& p : mp) {
+        if (p.second > n)
+            ans += n;
+        else ans += p.second;
     }
-    if ((sum % 2 != 0) or (o and e))
-        YES;
-    else NO;
-
-    return;
+    cout << ans << endl;
 }
 int main() {
     ios_base::sync_with_stdio(false);
