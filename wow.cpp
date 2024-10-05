05f9f5d6523066250adb46fda192063666f1c81a
SA Tahaa <satahaa12345@gmail.com>
2024-10-06 03:23:20 +0600

    updated primary layout

--- wow.cpp
+++ wow.cpp
@@ -9,8 +9,8 @@ typedef long long int i32;
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(). x.rend()
 #define MOD 1e9 + 7
-#define YES cout << "YES" << endl
-#define NO cout << "NO" << endl
+#define YES cout << "YES"
+#define NO cout << "NO"
 #define pb push_back
 constexpr int di[] = {-1, 1, 0, 0};
 constexpr int dj[] = {0, 0, -1, 1};
@@ -32,6 +32,6 @@ int main() {
     int tc = 1;
     cin >> tc;
     for (int i = 1; i <= tc; i++)
-        sol(i);
+        {sol(i); cout << endl;}
     return 0;
 }
