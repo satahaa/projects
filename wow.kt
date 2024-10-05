bf9a20d099497b91143e20eec18b72565c2d6d40
SA Tahaa <satahaa12345@gmail.com>
2024-10-06 02:00:18 +0600

    installed clion

--- wow.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val t = readLine()!!.toInt()
-    repeat(t) {
-        val (x, y) = readLine()!!.split(" ").map { it.toInt() }
-        val max = if (x > y) x else y
-        val min = if (x < y) x else y
-        println("$min $max")
-    }
-}
