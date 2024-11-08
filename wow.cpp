4eb259fc4c703dd01a079a503de42b580f5b95a2
SA Tahaa <satahaa12345@gmail.com>
2024-11-08 18:37:43 +0600

    icpc mock ans B

--- wow.cpp
+++ wow.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long int i32;
+typedef long long int ll;
 #define l(i, n) for (int i = 0; i < n; i++)
 #define sorted(a) sort(a.begin(), a.end())
 #define rsorted(a) sort(a.rbegin(), a.rend())
@@ -8,52 +8,54 @@ typedef long long int i32;
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define MOD 1000000007
-#define YES cout << "YES"
-#define NO cout << "NO"
+#define YES cout << "YES\n"
+#define NO cout << "NO\n" 
 #define spc ' '
 #define pb push_back
 constexpr int di[] = {-1, 1, 0, 0};
 constexpr int dj[] = {0, 0, -1, 1};
-
+bool is_rational(double x) {
+    x = std::abs(x);
+    for (int i = 0; i < 20; ++i) {
+        const auto a = std::floor(x);
+        if (x - a < 1e-8)
+            return true;
+        x = 1 / (x - a);
+    }
+    return false;
+}
 void sol(int tc) {
-    int q; cin >> q;
-    while (q--) {
-        i32 n, m, o, p;
-        cin >> n >> m >> o >> p;
-        i32 ans = 0;
+    char a, b;
+    cin >> a >> b;
 
-        vector<i32> powersOf2(m - n + 1);
-        vector<i32> powersOf3(p - o + 1);
+    double k = abs(a - b);
+    map<int, int> mp;
 
-        powersOf2[0] = 1;  // 2^0
-        for (int i = 1; i <= m - n; ++i) {
-            powersOf2[i] = (powersOf2[i - 1] * 2) % MOD;
-        }
-
-        powersOf3[0] = 1;  // 3^0
-        for (int j = 1; j <= p - o; ++j) {
-            powersOf3[j] = (powersOf3[j - 1] * 3) % MOD;
-        }
-
-        l (i, m - n + 1) {
-            l (j, p - o + 1) {
-                ans = (ans + (powersOf2[i] * powersOf3[j]) % MOD) % MOD;
+    if (k > 0) {
+        for (int i = 1; i <= (int)k; i++) {
+            double val = 94.0 / i;
+            if (is_rational(val)) {
+                mp[i] = static_cast<int>(ceil(val));
             }
         }
-        cout << 2 * ans << spc;
-    }
-}
 
+        for (auto [i, j] : mp) {
+            if (static_cast<int>(a) % i == static_cast<int>(b) % i)
+                cout << i << ' ' << j << '\n';
+        }
+    }
+    cout << '\n';
+}
+ 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
+ 
     int tc = 1;
-    //cin >> tc;
+    cin >> tc;
     for (int i = 1; i <= tc; i++) {
         sol(i);
-        cout << '\n';
     }
     return 0;
 }
