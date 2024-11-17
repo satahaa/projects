72b436f50dc5b53c8716aed605ea5a9bdb9a3cc5
SA Tahaa <satahaa12345@gmail.com>
2024-11-17 21:16:52 +0600

    2037B

--- wow.cpp
+++ wow.cpp
@@ -7,11 +7,12 @@ typedef long long int ll;
 #define umii unordered_map<ll, ll>
 #define si set<ll>
 #define usi unordered_set<ll>
-#define l(i, s, n) for (int i = s; i <= n; i++)
+#define ls(i, s, n) for (int i = s; i <= n; i++)
 #define rl(i, s, n) for (int i = s; i >= n; i--)
 #define l(i, n) for (int i = 0; i < n; i++)
-#define inp(v) for (auto &val : v) cin >> val
-#define acm(m) for (auto &[fst, snd] : m)
+#define inpv(v) for (auto &val : v) cin >> val
+#define outm(m) for (auto &[fst, snd] : m)
+#define outv(v) for (auto &val : v) cout << val << ' '
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
 #define Case cout << "Case " << tc << ": ";
@@ -26,7 +27,30 @@ typedef long long int ll;
 #define MOD 1000000007
 
 void sol(int tc) {
-    
+    int n;
+    cin >> n;
+    vi a(n);
+    umii mp;
+    l (i, n) {
+        cin >> a[i];
+        mp[a[i]]++;
+    }
+    int m = n - 2;
+    int sq = sqrt(m);
+    if (sq * sq == (m) && mp[sq] >= 2) {
+        cout << sq << spc << sq << nl;
+        return;
+    }
+    ls (i, 1, sqrt(m)) {
+        if ((m % i) == 0) {
+            if (mp[i] > 0 && mp[m / i] > 0) {
+                cout << i << spc << m / i << nl;
+                return;
+            }
+        }
+    }
+
+
 }
 
 int main() {
