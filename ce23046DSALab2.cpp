bba3e02863833f80ba9a822826d0f6d87261f13e
SA Tahaa <satahaa12345@gmail.com>
2024-11-20 01:34:08 +0600

    rm

--- ce23046DSALab2.cpp
+++ ce23046DSALab2.cpp
@@ -2,55 +2,43 @@
 using namespace std;
 int arr[] = {10, 7, 8, 9, 1, 5};
 int n = 6;
-void merge(int arr[], int left, int mid, int right) {
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-    int L[n1], R[n2];
-    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
-    for (int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
-    int i = 0, j = 0, k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i]; i++;
-        } else {
-            arr[k] = R[j]; j++;
-        }
-        k++;
-    }
-    while (i < n1) {
-        arr[k] = L[i];
-        i++; k++;
-    }
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; 
+    int i = low - 1; 
 
-    while (j < n2) {
-        arr[k] = R[j];
-        j++; k++;
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) { 
+            i++;
+            swap(arr[i], arr[j]); 
+        }
     }
+    swap(arr[i + 1], arr[high]); 
+    return i + 1;               
 }
 
-void mergeSort(int arr[], int left, int right) {
-    if (left < right) {
-        int mid = left + (right - left) / 2;
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high); 
 
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-
-        merge(arr, left, mid, right);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
 int main() {
 
     cout << "Original Array: ";
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
+    }
     cout << endl;
 
-    mergeSort(arr, 0, n - 1);
+    quickSort(arr, 0, n - 1);
 
     cout << "Sorted Array: ";
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
+    }
     cout << endl;
 
     return 0;
